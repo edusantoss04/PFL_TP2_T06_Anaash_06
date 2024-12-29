31eca5527a2fdf14e32c84e33a2f1cb0255ca057
PP <pedrosspedro@sapo.pt>
2024-12-29 16:43:40 +0000

    game_over done

--- src/a.pl
+++ src/a.pl
@@ -29,7 +29,7 @@ board(10, [[blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1, 
            [blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1, red-1],
            [red-1, blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1, red-1, blue-1]]).
 
-move(gameState(Board, Player), move(Row, Col, ToRow, ToCol), gameState(NewBoard, NewPlayer)) :-
+move(gameState(Board, Player, _, _, _, _), move(Row, Col, ToRow, ToCol), gameState(NewBoard, NewPlayer, _, _, _, _)) :-
     % Trocar o jogador
     next_player(Player, NewPlayer),
 
@@ -116,15 +116,15 @@ replace([H|T], N, X, [H|R]) :-  % Caso contrário, percorre a lista
 
 
 
-% para testar move(gameState([[blue-1, red-1], [red-1, blue-1]], blue), move(0, 0, 1, 0), gameState(NewBoard, NewPlayer)).
+% para testar move(gameState([[blue-1, red-1], [red-1, blue-1]], blue, _, _, _, _), move(0, 0, 1, 0), gameState(NewBoard, NewPlayer, _, _, _, _)).
 
-% gameState(board, player).
+% gameState(board, player, GameType, RedType, BlueType, Level).
 
 % gameConfig(GameType,SizeBoard, Dificulty).
 
-% para testar display_board(gameState(board(6,[[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1],[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1],[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1]]),_)).
+% para testar display_game(gameState(board(6,[[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1],[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1],[blue-1, red-1, blue-1, red-1, blue-1, red-1],[red-1, blue-1, red-1, blue-1, red-1, blue-1]]), _, _, _, _, _)).
 
-display_board(gameState(board(BoardSize, Board), _)):-
+display_game(gameState(board(BoardSize, Board), _, _, _, _, _)):-
     print_header(BoardSize),
     print_lines(Board, 0).
     
@@ -164,3 +164,36 @@ print_dashes(N) :-
     write('---------+'),
     N1 is N - 1,
     print_dashes(N1).
+
+
+% testar game_over(gameState([[red-2, blue-1, red-1, red-1, red-1, red-1],[red-1, red-1, red-1, red-1, red-1, red-1]],_,_,_,_,_), Winner).
+
+% Caso base: game over se todas as peças são da mesma cor.
+game_over(gameState(Board, _, _, _, _,_), Winner) :-
+    flatten(Board, FlatList),           % Achatar a lista de listas em uma lista única.
+    exclude(=(empty), FlatList, Pieces), % Remover todas as posições 'empty'.
+    same_color(Pieces, Winner).         % Verificar se todas as peças têm a mesma cor.
+
+% Predicado que verifica se todas as peças têm a mesma cor (ignorando números).
+same_color([], _) :- fail.              % Falha se não houver peças (apenas 'empty').
+same_color([Color-_|T], Winner) :-      % Ignora o número e verifica apenas a cor.
+    maplist(has_same_color(Color), T),  % Verifica se todos os elementos têm a mesma cor.
+    Winner = Color.
+
+% Verifica se a peça tem a mesma cor (ignora números).
+has_same_color(Color, Color-_).
+
+% Caso base: uma lista vazia já está achatada.
+flatten([], []).
+
+% Se o primeiro elemento for uma lista, achatar recursivamente.
+flatten([H|T], FlatList) :-
+    is_list(H),
+    flatten(H, HFlat),
+    flatten(T, TFlat),
+    append(HFlat, TFlat, FlatList).
+
+% Se o primeiro elemento não for uma lista, apenas adicioná-lo ao resultado.
+flatten([H|T], [H|TFlat]) :-
+    \+ is_list(H),
+    flatten(T, TFlat).
